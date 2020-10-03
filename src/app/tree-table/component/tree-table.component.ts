42ca9b5ff2f5df5eedf821abd37f34db5a24dcd8
XareeN <xareen@interia.eu>
2020-10-03 16:41:55 +0200

    basic table implemented

--- src/app/tree-table/component/tree-table.component.ts
+++ src/app/tree-table/component/tree-table.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BASIC_FLAT_DATA} from '../mock/data-mock';
 
 @Component({
   selector: 'app-tree-table, tree-table',
   templateUrl: './tree-table.component.html',
-  styleUrls: ['./tree-table.component.scss']
+  styleUrls: ['./tree-table.component.scss'],
 })
 export class TreeTableComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = BASIC_FLAT_DATA;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
