b1f37201a901c9a59eb11b1537400b52d1863aee
XareeN <xareen@interia.eu>
2020-10-03 11:35:48 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TreeTableModule } from './tree-table/tree-table.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tree-table/tree-table.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TreeTableComponent } from './component/tree-table.component';
+
+
+
+@NgModule({
+  declarations: [TreeTableComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TreeTableModule { }
